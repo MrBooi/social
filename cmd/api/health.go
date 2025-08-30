4228f00c0f205220a8c12bc06e27f23b646743d3
MrBooi <ayabongabooi2@gmail.com>
2025-08-30 22:27:57 +0200

    chore: change port back to 8080

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -3,8 +3,6 @@ package main
 import "net/http"
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("OK"))
-	if err != nil {
-		return
-	}
+	w.Write([]byte("OK"))
+
 }
