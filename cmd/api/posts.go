ca4a52746102120fbd706970c3bddad54f3e0b23
MrBooi <ayabongabooi2@gmail.com>
2025-09-09 20:18:35 +0200

    wip: add validator package and handle http payload validation

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -10,17 +10,20 @@ import (
 )
 
 type CreatePostPayload struct {
-	//ID      int64    `json:"id"`
-	Content string `json:"content"`
-	Title   string `json:"title"`
-	//UserID  int64    `json:"user_id"`
-	Tags []string `json:"tags"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
+	Tags    []string `json:"tags"`
 }
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
 
 	if err := readJSON(w, *r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
